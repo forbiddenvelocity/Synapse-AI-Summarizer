ff2633c3a8b5315219362616730aa4da9d7dc8ab
Mubashir <mubashir1728@gmail.com>
2023-07-02 13:58:19 +0400

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from '../services/article';
+
+const Demo = () => {
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+      });
+
+      const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+      const handleSubmit = async (e) => {
+        e.preventDefault()
+        const { data } = await getSummary({ articleUrl: article.url });
+        if(data?.summary) {
+            const newArticle = {...article, summary: data.summary }
+
+            setArticle(newArticle)
+
+            console.log(newArticle)
+        }
+      }
+
+      const handleKeyDown = (e) => {
+        if (e.keyCode === 13) {
+          handleSubmit(e);
+        }
+      };
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
+            required
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        </div>
+    </section>
+
+  )
+}
+
+export default Demo
---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>Synapse AI</span>
+      </h1>
+      <h2 className='desc'>
+      Streamline your reading experience with Synapse Summarizer, an open-source article summarizer that condenses extensive articles into succinct and comprehensible summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
